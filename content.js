e4bfe3af2812ae93adca9721f0d42dffb3400bdb
F. Navajas <5147072+kiskonr@users.noreply.github.com>
2021-06-17 17:43:14 +0200

    fix: new google meet ui

--- content.js
+++ content.js
@@ -1,20 +1,25 @@
 (async () => {
-  const src = chrome.runtime.getURL('./helper.js');
+  const src = chrome.runtime.getURL("./helper.js");
   const helper = await import(src);
 
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'randomize') {
+    if (request.action === "randomize") {
       if (request.participants !== null) {
         setTimeout(() =>
           sendResponse({ data: helper.shuffleArray(request.participants) })
         );
       } else {
         (async () => {
-          const showParticipantsButton = document.querySelector(
-            '*[data-tab-id~="1"]'
-          );
+          const showParticipantsButton =
+            document.querySelector('button[data-panel-id~="1"]') ||
+            document.querySelector('*[data-tab-id~="1"]');
+
           if (showParticipantsButton) {
-            showParticipantsButton.click();
+            if (
+              showParticipantsButton.getAttribute("aria-pressed") !== "true"
+            ) {
+              showParticipantsButton.click();
+            }
             await helper.loadAllParticipants();
             sendResponse({ data: helper.randomizeParticipants() });
           }
